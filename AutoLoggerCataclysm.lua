0531a2ac56493bd6e9a16616ddc79795497b4c88
Ryp <ryp.erl@pm.me>
2023-10-11 05:42:12 +0200

    chore: initial commit

---
+++ AutoLoggerCataclysm.lua
@@ -0,0 +1,62 @@
+AutoLoggerCataclysm = AutoLoggerCataclysm or
+    LibStub("AceAddon-3.0"):NewAddon("AutoLoggerCataclysm", "AceConsole-3.0", "AceEvent-3.0")
+
+AutoLoggerCataclysm.name = "AutoLoggerCataclysm"
+AutoLoggerCataclysm.version = "1.0.0"
+
+AutoLoggerCataclysm.raids = {
+    -- Baradin Hold
+    [757] = true,
+    -- Blackwing Descent
+    [669] = true,
+    -- The Bastion of Twilight
+    [671] = true,
+    -- Throne of the Four Winds
+    [754] = true,
+    -- Firelands
+    [720] = true,
+    -- Dragon Soul
+    [967] = true,
+}
+--[[
+    Initializing the addon using Ace3.
+    It includes options, commands, saved variables and basic events to register to.
+--]]
+
+function AutoLoggerCataclysm:OnInitialize()
+end
+
+function AutoLoggerCataclysm:OnEnable()
+    self:RegisterEvent("UPDATE_INSTANCE_INFO")
+    AutoLoggerCataclysm:CheckInstanceAndLog()
+end
+
+function AutoLoggerCataclysm:OnDisable()
+end
+
+--[[
+    Events
+--]]
+
+function AutoLoggerCataclysm:UPDATE_INSTANCE_INFO()
+    AutoLoggerCataclysm:CheckInstanceAndLog()
+end
+
+--[[
+    Core functions
+--]]
+
+function AutoLoggerCataclysm:CheckInstanceAndLog()
+    local name, _, _, _, _, _, _, instanceID, instanceGroupSize, _ = GetInstanceInfo()
+    if AutoLoggerCataclysm.raids[tonumber(instanceID)] then
+        if not LoggingCombat() then
+            LoggingCombat(true)
+            self:Print("Combat logging |cff33ff00activated|r for " .. name .. " (" .. instanceGroupSize .. ").")
+        end
+    else
+        if LoggingCombat() then
+            self:Print("Combat logging |cffff0000deactivated|r (not in a WotLK raid).")
+            LoggingCombat(false)
+        end
+    end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# AutoLoggerCataclysm
+
+Automatically start combat logging when entering a Cataclysm raid:
+
+- Baradin Hold
+- Blackwing Descent
+- The Bastion of Twilight
+- Throne of the Four Winds
+- Firelands
+- Dragon Soul
+
+[GNU GPLv3](LICENSE)
